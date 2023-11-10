8222b26b5c39c6f34bc60e7776c142d1fcc72ff9
Pavel Sheshko <psheshko@twelvedevs.com>
2023-11-10 18:10:45 +0300

    feat: case section

---
+++ app/components/MainPage/Case/index.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import caseBlue from "@/public/images/airpods-max-smartcase-case_blue.png";
+
+export const CaseMain = () => {
+  return (
+    <section className="case">
+      <div className="container">
+        <h2 className="case__title main__title">
+          It even preserves the battery.
+        </h2>
+        <p className="case__text">
+          When stored in the sleek and lightweight{" "}
+          <span className="case__bold">Smart Case</span>, AirPods Max enter a
+          low-power state to save energy.
+        </p>
+        <Image className="case__image" src={caseBlue} alt="blue case" />
+        <h2 className="case__subtitle main__title">
+          Stayin' alive, stayin' alive.
+        </h2>
+        <p className="case__numbers">20</p>
+        <p className="case__subtext">
+          hours of music, movies and calls with Active Noise Cancellation and
+          Custom Spatial Audio turned on. 5 And just 5 minutes of charging via
+          the Lightning connector gives you an hour and a half of sound. 6
+        </p>
+      </div>
+    </section>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 
-import { ColorMain, CompositionMain } from "./components";
+import { CaseMain, ColorMain, CompositionMain } from "./components";
 
 export default function Home() {
   return (
     <main className="main">
       <ColorMain />
       <CompositionMain />
+      <CaseMain />
     </main>
   );
 }
